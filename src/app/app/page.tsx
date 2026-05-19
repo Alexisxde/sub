6f067a1f803a281804034cde460fa3c88e4959a6
Olivarez Alexis <112827890+Alexisxde@users.noreply.github.com>
2026-05-19 22:46:04 +0000

    feat(app): wrap subscription calendar in Suspense

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,9 +1,12 @@
 import SubscriptionCalendar from "@/features/subscription/components/subscription-calendar"
+import { Suspense } from "react"
 
 export default function Page() {
 	return (
 		<section className="h-dvh p-4 pb-8 md:p-8 md:pr-8">
-			<SubscriptionCalendar />
+			<Suspense>
+				<SubscriptionCalendar />
+			</Suspense>
 		</section>
 	)
 }
