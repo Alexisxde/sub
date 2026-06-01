cec048283bff2704aefe023d8cd9e8e0809da3c3
Olivarez Alexis <112827890+Alexisxde@users.noreply.github.com>
2026-06-01 19:27:55 +0000

    refactor(ui): Remove SidebarProvider from app layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,12 +1,10 @@
-import Sidebar, { SidebarProvider } from "@/components/sidebar"
+import Sidebar from "@/components/sidebar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<SidebarProvider>
-			<div className="flex gap-4 h-dvh">
-				<Sidebar />
-				<main className="flex-1 overflow-auto">{children}</main>
-			</div>
-		</SidebarProvider>
+		<section className="flex gap-4 h-dvh">
+			<Sidebar />
+			<main className="flex-1 overflow-auto">{children}</main>
+		</section>
 	)
 }
